0c5b3eddb1e30b7bbb58cc66d30b5a497e18f4fc
Pavlo <p.kalashnikov@epages.com>
2020-12-31 18:21:48 +0100

    user creation

--- PizzaService_Straesser_Baitakova/src/main/java/whz/pti/eva/pizzaService/config/WebSecurityConfig.java
+++ PizzaService_Straesser_Baitakova/src/main/java/whz/pti/eva/pizzaService/config/WebSecurityConfig.java
@@ -1,14 +1,10 @@
 package whz.pti.eva.pizzaService.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -17,16 +13,19 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 // add public available endpoints
                 .authorizeRequests()
-                .antMatchers("/signin","/signup", "/css/**", "/menu", "/h2-console/**")
+                .antMatchers("/signin",
+                        "/signup",
+                        "/css/**",
+                        "/menu",
+                        "/h2-console/**",
+                        "/users/create"
+                )
                 .permitAll()
 
                 .anyRequest().authenticated()
@@ -47,13 +46,13 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .logoutSuccessUrl("/logout.done");
 
-                
+
         http.csrf().disable();
         http.headers().frameOptions().disable();
     }
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-    	return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- PizzaService_Straesser_Baitakova/src/main/java/whz/pti/eva/pizzaService/security/boundary/UserController.java
+++ PizzaService_Straesser_Baitakova/src/main/java/whz/pti/eva/pizzaService/security/boundary/UserController.java
@@ -1,35 +1,36 @@
 package whz.pti.eva.pizzaService.security.boundary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import whz.pti.eva.pizzaService.security.domain.Customer;
 import whz.pti.eva.pizzaService.security.domain.CustomerCreateForm;
 import whz.pti.eva.pizzaService.security.service.customer.CustomerService;
-import whz.pti.eva.pizzaService.security.service.customer.CustomerServiceImpl;
 
 @Controller
 public class UserController {
-	
-	private CustomerService customerService;
-	
-	@Autowired
-	public UserController(CustomerService customerService) {
-		this.customerService = customerService;
-	}
 
-	@RequestMapping(value = {"/signup"}, method = RequestMethod.GET)
-	public String getRegistrationPage() {
-		return "signUp";
-	}
-	
-	@RequestMapping(value = {"/users/create"}, method = RequestMethod.POST)
-	public String signUpUser(CustomerCreateForm model){
-		Customer customer =  customerService.create(model); 
-		System.out.println(customer);
-		return "signIn";
-	}
-	
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private CustomerService customerService;
+
+    @Autowired
+    public UserController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping(value = {"/signup"}, method = RequestMethod.GET)
+    public String getRegistrationPage() {
+        return "signUp";
+    }
+
+    @RequestMapping(value = {"/users/create"}, method = RequestMethod.POST)
+    public String signUpUser(CustomerCreateForm model) {
+        Customer customer = customerService.create(model);
+        log.info("USER CREATED.");
+        return "signIn";
+    }
+
 }
