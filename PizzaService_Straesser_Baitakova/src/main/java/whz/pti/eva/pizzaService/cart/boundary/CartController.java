95168a518cfe8ab1cc8569e7689ce3587334eb20
Pavlo <p.kalashnikov@epages.com>
2021-01-03 21:43:34 +0100

    signup works

--- PizzaService_Straesser_Baitakova/src/main/java/whz/pti/eva/pizzaService/cart/boundary/CartController.java
+++ PizzaService_Straesser_Baitakova/src/main/java/whz/pti/eva/pizzaService/cart/boundary/CartController.java
@@ -1,16 +1,9 @@
 package whz.pti.eva.pizzaService.cart.boundary;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import whz.pti.eva.pizzaService.cart.domain.Cart;
 import whz.pti.eva.pizzaService.cart.service.CartService;
 import whz.pti.eva.pizzaService.common.CurrentUserUtil;
@@ -20,44 +13,50 @@ import whz.pti.eva.pizzaService.order.service.OrderedItemService;
 import whz.pti.eva.pizzaService.order.service.OrderedService;
 import whz.pti.eva.pizzaService.pizza.domain.Item;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 
 @Controller
 public class CartController {
-	private CartService cartService;
-	private OrderedService orderedService;
-	private OrderedItemService orderedItemService;
-	@Autowired
-	 public CartController(CartService cartService, OrderedService orderedService, OrderedItemService orderedItemService) {
-		 this.cartService = cartService;
-		 this.orderedService = orderedService;
-		 this.orderedItemService = orderedItemService;
-	 }
-	
-	 @RequestMapping(value ="/cart")
-	 public String listAllPizza(Model model) {
-		 Long customerID = CurrentUserUtil.getCurrentUserID(model);
-		 Optional<Cart> cart = cartService.getCart(customerID);
-		 cart.ifPresent( cartTemp -> {
-			 List<Item> items = cartTemp.getItemList();
-			 BigDecimal totalPrice = BigDecimal.valueOf(items.stream().collect(Collectors.summingDouble(item -> item.getPrice().doubleValue())));
-			 model.addAttribute("totalPrice", totalPrice);
-			 model.addAttribute("listOfAllItems", items);
-		 });
-		 return "cart";
-	 }
-	 
-	 @RequestMapping(value ="/order")
-	 public String order(Model model) {
-		 Long customerID = CurrentUserUtil.getCurrentUserID(model);
-		 Cart cart = cartService.getCart(customerID).get();
-		 Ordered ordered = new Ordered();
-		 ordered.setUserId(customerID);
-		 cart.getItemList().forEach(item -> {
-			 OrderedItem oItem = orderedItemService.newOrderedItem(item.getPizza().getPizzaName(), item.getPizza().getId(), item.getQuantity(), customerID, item.getSize());
-			 orderedService.addOreredItem(ordered, oItem);
-		 });
-		 cartService.clearCart(cart);
-		 return "redirect:menu";
-	 }
-	 
+    private CartService cartService;
+    private OrderedService orderedService;
+    private OrderedItemService orderedItemService;
+
+    @Autowired
+    public CartController(CartService cartService, OrderedService orderedService, OrderedItemService orderedItemService) {
+        this.cartService = cartService;
+        this.orderedService = orderedService;
+        this.orderedItemService = orderedItemService;
+    }
+
+    @RequestMapping(value = "/cart")
+    public String listAllPizza(Model model) {
+        Long customerID = CurrentUserUtil.getCurrentUserID(model);
+        Optional<Cart> cart = cartService.getCart(customerID);
+        cart.ifPresent(cartTemp -> {
+            List<Item> items = cartTemp.getItemList();
+            BigDecimal totalPrice = BigDecimal
+                    .valueOf(items.stream().mapToDouble(item -> item.getPrice().doubleValue()).sum());
+            model.addAttribute("totalPrice", totalPrice);
+            model.addAttribute("listOfAllItems", items);
+        });
+        return "cart";
+    }
+
+    @RequestMapping(value = "/order")
+    public String order(Model model) {
+        Long customerID = CurrentUserUtil.getCurrentUserID(model);
+        Cart cart = cartService.getCart(customerID).get();
+        Ordered ordered = new Ordered();
+        ordered.setUserId(customerID);
+        cart.getItemList().forEach(item -> {
+            OrderedItem oItem = orderedItemService.newOrderedItem(item.getPizza().getPizzaName(), item.getPizza().getId(), item.getQuantity(), customerID, item.getSize());
+            orderedService.addOreredItem(ordered, oItem);
+        });
+        cartService.clearCart(cart);
+        return "redirect:menu";
+    }
+
 }
--- PizzaService_Straesser_Baitakova/src/main/java/whz/pti/eva/pizzaService/security/boundary/UserController.java
+++ PizzaService_Straesser_Baitakova/src/main/java/whz/pti/eva/pizzaService/security/boundary/UserController.java
@@ -1,7 +1,5 @@
 package whz.pti.eva.pizzaService.security.boundary;
 
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,50 +11,65 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import whz.pti.eva.pizzaService.cart.domain.Cart;
+import whz.pti.eva.pizzaService.cart.domain.CartRepository;
 import whz.pti.eva.pizzaService.pizza.boundary.PizzaController;
 import whz.pti.eva.pizzaService.security.domain.Customer;
 import whz.pti.eva.pizzaService.security.domain.CustomerCreateForm;
 import whz.pti.eva.pizzaService.security.domain.Role;
 import whz.pti.eva.pizzaService.security.service.customer.CustomerService;
-import whz.pti.eva.pizzaService.security.service.customer.CustomerServiceImpl;
 import whz.pti.eva.pizzaService.security.service.validation.UserCreateFormValidator;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
-	private static final Logger log = LoggerFactory.getLogger(PizzaController.class);
-	private CustomerService customerService;
-	private UserCreateFormValidator userCreateFormValidator;
-	
-	@Autowired
-	public UserController(CustomerService customerService, UserCreateFormValidator userCreateFormValidator) {
-		this.customerService = customerService;
-		this.userCreateFormValidator = userCreateFormValidator;
-	}
-	
-	@InitBinder("myform")
+    private static final Logger log = LoggerFactory.getLogger(PizzaController.class);
+    private CustomerService customerService;
+    private UserCreateFormValidator userCreateFormValidator;
+    private CartRepository cartRepository;
+
+    @Autowired
+    public UserController(
+            CustomerService customerService,
+            UserCreateFormValidator userCreateFormValidator,
+            CartRepository cartRepository
+    ) {
+        this.customerService = customerService;
+        this.userCreateFormValidator = userCreateFormValidator;
+        this.cartRepository = cartRepository;
+    }
+
+    @InitBinder("myform")
     public void initBinder(WebDataBinder binder) {
         binder.addValidators(userCreateFormValidator);
     }
-	
-	@RequestMapping(value = {"/signup"}, method = RequestMethod.GET)
-	public String getRegistrationPage() {
-		return "signUp";
-	}
-	
-	@RequestMapping(value = {"/users/create"}, method = RequestMethod.POST)
-	public String signUpUser(@Valid @ModelAttribute("myform") CustomerCreateForm form,
-			BindingResult bindingResult, Model model){
-		if (bindingResult.hasErrors()) {
+
+    @RequestMapping(value = {"/signup"}, method = RequestMethod.GET)
+    public String getRegistrationPage() {
+        return "signUp";
+    }
+
+    @RequestMapping(value = {"/users/create"}, method = RequestMethod.POST)
+    public String signUpUser(
+            @Valid @ModelAttribute("myform") CustomerCreateForm form,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
             model.addAttribute("error", bindingResult.getGlobalError().getDefaultMessage());
             return "signUp";
         }
-		
-		customerService.create(form); 
-		if(customerService.getCustomerByLoginName(form.getLoginName()).get().getRole()==null) {
-			customerService.getCustomerByLoginName(form.getLoginName()).get().setRole(Role.USER);
-		}
-		return "signIn";
-	}
-	
+
+        Customer customer = customerService.create(form);
+        Cart cart = new Cart(customer);
+        cartRepository.save(cart);
+
+        if (customerService.getCustomerByLoginName(form.getLoginName()).get().getRole() == null) {
+            customerService.getCustomerByLoginName(form.getLoginName()).get().setRole(Role.USER);
+        }
+
+        return "signIn";
+    }
+
 }
